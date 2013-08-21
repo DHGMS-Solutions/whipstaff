f02661f30bc736bf7d61f7482ca963186e318d2e
dpvreony_cp <dpvreony@hotmail.com>
2013-08-21 14:59:22 +0000

    update of reactiveui
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs01]$/whipstaff/main;C103716

---
+++ Dhgms.Whipstaff/xDhgms.Whipstaff/ViewModel/Interface/IPermissionsListViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Dhgms.Whipstaff.ViewModel
+{
+    using System.Collections.Generic;
+
+    using ReactiveUI;
+
+    /// <summary>
+    /// Interface for the permissions list view model
+    /// </summary>
+    public interface IPermissionsListViewModel : IRoutableViewModel
+    {
+        /// <summary>
+        /// List of groups the user belongs to
+        /// </summary>
+        IList<string> Groups { get; set; }
+
+        /// <summary>
+        /// List of permissions the user has (role based)
+        /// </summary>
+        IList<string> Permissions { get; set; }
+    }
+}
