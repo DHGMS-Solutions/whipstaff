74b103ff9895edf9e8a21240df21fa8e98d8cbf9
dpvreony_cp <02013d7bceb8ac992f5fe8999e13b687462345ebjsft5nxs>
2013-08-22 10:20:59 +0000

    restructure of desktop showcase app. addition of akavache
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs01]$/whipstaff/main;C103723

---
+++ Dhgms.Whipstaff/Dhgms.Whipstaff/ViewModel/Base.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dhgms.Whipstaff.ViewModel
+{
+    using ReactiveUI;
+
+    public class Base : ReactiveObject, IRoutableViewModel, IFeatureUsageTracking
+    {
+        /// <summary>
+        /// Gets or sets a string token representing the current ViewModel, such as 'login' or 'user'
+        /// </summary>
+        public string UrlPathSegment { get; protected set; }
+
+        /// <summary>
+        /// Gets the IScreen that this ViewModel is currently being shown in. This
+        ///             is usually passed into the ViewModel in the Constructor and saved
+        ///             as a ReadOnly Property.
+        /// </summary>
+        public IScreen HostScreen { get; private set; }
+
+        public void OnFeatureStart()
+        {
+        }
+
+        public void OnFeatureEnd()
+        {
+        }
+
+        public void OnFeatureException()
+        {
+        }
+
+        public void OnFeatureError()
+        {
+        }
+    }
+}
