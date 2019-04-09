c71a4e59bb16690c28fe8891519af61b42555a58
David Vreony <dpvreony@users.noreply.github.com>
2019-04-09 20:14:32 +0100

    showcase prep

---
+++ src/Dhgms.Whipstaff.Core/ViewModels/IAddItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Reactive;
+using ReactiveUI;
+
+namespace Dhgms.Whipstaff.Core.ViewModels
+{
+    public interface IAddItemViewModel<T> : IReactiveObject
+    {
+        ReactiveCommand<Unit, T> Add { get; }
+    }
+}
---
+++ src/Dhgms.Whipstaff.Core/ViewModels/IListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ReactiveUI;
+
+namespace Dhgms.Whipstaff.Core.ViewModels
+{
+    public interface IListViewModel<T> : IReactiveObject
+    {
+        DynamicData.IObservableList<T> Items { get; }
+    }
+}
